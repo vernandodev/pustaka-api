eea1d68e7418b159f4ded77927a98d4eef304ba8
vernandodev <Richovrnando@gmail.com>
2022-10-28 14:38:33 +0700

    api

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+
+	router.POST("/books", postBooksHandler)
+
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Richo",
+		"desc": "A Software Engineer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Hello",
+		"desc": "Hello",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
+	})
+}
+
+type BookInput struct {
+	Title string `json:"title" binding:"required"`
+	Price int    `json:"price" binding:"required,number"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	// title, price
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": bookInput.Title,
+		"price": bookInput.Price,
+	})
+
+}
